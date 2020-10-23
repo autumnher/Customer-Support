9c849c1762125773f7a935a2072bc207f7ff0a72
Autumn Her <herr.autumn@gmail.com>
2020-10-23 16:24:29 -0400

    MyMessage

--- src/main/java/com/uncc/ticket/TicketApplication.java
+++ src/main/java/com/uncc/ticket/TicketApplication.java
@@ -7,8 +7,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @SpringBootApplication
 public class TicketApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(TicketApplication.class, args);
-    }
+    public static void main(String[] args) { SpringApplication.run(TicketApplication.class, args); }
 
 }
--- src/main/java/com/uncc/ticket/controller/TicketController.java
+++ src/main/java/com/uncc/ticket/controller/TicketController.java
@@ -20,6 +20,7 @@ public class TicketController {
     private TicketService ticketService;
 
     private final UsersService usersService;
+    private Object s;
 
     @Autowired
     public TicketController(TicketService ticketService, UsersService usersService) {
@@ -53,12 +54,15 @@ public class TicketController {
     @RequestMapping(value = "/tickets/edit/{id}", method = RequestMethod.GET)
     public String editTicket(Model model,@PathVariable("id") Long id) {
         // Code here
-        return "redirect:/"; //Remove this line
+        model.addAttribute("ticket", ticketService.findById(id));
+        return "tickets/storeTicket";
     }
 
     @RequestMapping(value = "/tickets/delete/{id}", method = RequestMethod.GET)
     public String deleteTicket(@PathVariable("id") Long id) {
         // Code here
+       ticketService.deleteById(id);
+
         return "redirect:/";
     }
 
